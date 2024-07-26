e4f3b069081c92774cacefe666235866a6077d1c
David Swatzell <thatsstrikethree@gmail.com>
2024-07-26 15:52:17 -0400

    add uselocation to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,29 @@
 import './NavBar.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/search">Search</Link>
-        </li>
-        <li>
-          <Link to="/favorites">Favorites</Link>
-        </li>
+        {currentPath !== '/' && (
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        )}
+        {currentPath !== '/search' && (
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+        )}
+        {currentPath !== '/favorites' && (
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
