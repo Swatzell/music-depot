be75f26ddc55369b7d2e43a8d5948e262e59a541
David Swatzell <thatsstrikethree@gmail.com>
2024-07-29 14:07:04 -0400

    refactor home for error handling and add sad path tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,4 +1,4 @@
-describe('Home Page', () => {
+describe('Home Page happy path', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://api.discogs.com/artists/**', { fixture: 'artist.json' }).as('getFeaturedArtist');
     cy.visit('http://localhost:3000/');
@@ -10,5 +10,21 @@ describe('Home Page', () => {
     cy.contains('Featured Artist');
 
     cy.get('div.MuiCard-root').should('exist');
+  });
+});
+
+describe('Home Page Sad Path', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://api.discogs.com/artists/**', { statusCode: 500 }).as('getFeaturedArtistError');
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should handle API failure', () => {
+    cy.wait('@getFeaturedArtistError');
+    cy.contains('Welcome to Music Depot');
+    cy.contains('Featured Artist');
+
+    cy.get('div.MuiCard-root').should('not.exist');
+    cy.contains('Error loading featured artist').should('exist');
   });
 });
