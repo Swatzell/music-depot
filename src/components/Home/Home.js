edbfe9ff2252cef2f43ca27c7b8ba1f397d5f890
David Swatzell <thatsstrikethree@gmail.com>
2024-07-29 10:46:08 -0400

    add random featured artist on main page and styled it using material ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+
+const artistIds = [125246, 605148, 17199];
 
 function Home() {
+  const [artist, setArtist] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchRandomArtist = async () => {
+      const token = 'kqgYPvozoTdGUTWqjCHfEUpjCUuQOuBsJIzNiIoH';
+      const randomArtistId = artistIds[Math.floor(Math.random() * artistIds.length)];
+      const url = `https://api.discogs.com/artists/${randomArtistId}?token=${token}`;
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setArtist(data);
+      } catch (error) {
+        console.error('Error fetching artist details:', error);
+      }
+    };
+
+    fetchRandomArtist();
+  }, []);
+
+  const handleCardClick = () => {
+    navigate(`/artist/${artist.id}`);
+  };
+
   return (
     <div>
       <h1>Music Depot</h1>
-      
+      {artist && (
+        <Card>
+          <CardActionArea onClick={handleCardClick}>
+            <CardMedia
+              component="img"
+              height="500"
+              image={artist.images[0].uri}
+              alt={artist.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {artist.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
     </div>
   );
 }
