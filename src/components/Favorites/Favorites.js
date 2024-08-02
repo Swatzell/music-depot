1667cdc692c23cfeeab46cdcac48e32caa9e2462
David Swatzell <thatsstrikethree@gmail.com>
2024-08-02 15:16:38 -0400

    refactor and add a deault image for when there isnt one provided by the api

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FavoritesContext } from '../../contexts/FavoritesContext';
 import { Container, Box, Typography, Grid, Card, CardActionArea, CardContent, CardMedia } from '@mui/material';
+import defaultImage from '../../assets/karim-boubker-inCiuLNuwdw-unsplash.jpg';
 
 function Favorites() {
   const { favorites } = useContext(FavoritesContext);
@@ -29,14 +30,12 @@ function Favorites() {
               <Grid item xs={12} sm={6} md={4} key={index}>
                 <Card onClick={() => handleResultClick(artist.id)}>
                   <CardActionArea>
-                    {artist.image && (
-                      <CardMedia
-                        component="img"
-                        height="200"
-                        image={artist.image}
-                        alt={artist.name}
-                      />
-                    )}
+                    <CardMedia
+                      component="img"
+                      height="200"
+                      src={artist.image || defaultImage} 
+                      alt={artist.name}
+                    />
                     <CardContent>
                       <Typography gutterBottom variant="h6" component="div">
                         {artist.name}
